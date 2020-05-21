dac6b5fe064ca733d3a7b4d1ba084420ca690ad8
Philipp Wollschlegel <folken@kabelsalat.ch>
2020-05-21 21:56:33 +0000

    use vars

--- doit.sh
+++ doit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-./01_convertgz switzerland-latest
-./02_convertformat switzerland-latest
-./03_dropauthor switzerland-latest
-./04_filtero5m switzerland-latest
-./05_converttoshp switzerland-latest
+if [ -z "$1" ]; then
+  echo specify file without extension
+  exit 1
+fi
+./01_convertgz $1
+./02_convertformat $1
+./03_dropauthor $1
+./04_filtero5m $1
+./05_converttoshp $1
 ./zz_packup
 ./zz_test
