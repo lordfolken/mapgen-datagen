9fb05569b2c2171625e90ee8f891dc3a73dc05fe
Philipp Wollschlegel <folken@kabelsalat.ch>
2020-06-24 03:41:58 +0200

    current state, working

--- reduce.sh
+++ reduce.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-v.in.ogr -t input=./ layer=$1_line output=$1_line1
-v.generalize input=$1_line1 output=$1_line_reduced method=douglas_reduction threshold=0 reduction=30
 
-v.in.ogr -t input=./ layer=$1_area output=$1_area1
-v.generalize input=$1_area1 output=$1_area_reduced method=douglas_reduction threshold=0 reduction=30
+export LC_ALL=en_US.utf8
+
+case "$( basename $0 )" in
+  reduce_line.sh) 
+g.remove type=vector name=$1_line1,$1_line2,$1_line3,$1_line4,$1_line5,$1_line6
+v.in.ogr -t input=./ layer=$1_line output=$1_line1
+v.generalize input=$1_line1 output=$1_line2 method=douglas threshold=0.0002
+#v.build.polylines input=$1_line2 output=$1_line3
+#v.clean input=$1_line1 output=$1_line2 tool=snap,break,rmdupl,rmbridge,rmdangle thres=0.002
+v.clean input=$1_line2 output=$1_line3 tool=rmdupl,rmbridge,rmdangle thres=0.002
+v.out.ogr input=$1_line3 type=line output=$2/$1_line_redux.shp format=ESRI_Shapefile
+  ;;
+  reduce_area.sh) 
+g.remove type=vector name=$1_area1,$1_area2,$1_area3,$1_area4,$1_area5,$1_area6
+v.in.ogr -t input=./ layer=$1_area output=$1_area1 snap=1e-03
+#v.build.polylines input=$1_area1 output=$1_area2
+v.clean input=$1_area1 output=$1_area2 tool=rmdangle,rmsa,rmdupl,rmbridge --verbose
+#v.generalize input=$1_area2 output=$1_area3 method=network threshold=100
+#v.dissolve input=$1_area3 output=$1_area4
+#v.clean input=$1_area4 output=$1_area5 tool=rmarea threshold=100000
+#v.edit map=$1_area1 type=boundary tool=delete query=length thresh=0,0,-0.5
+#v.clean input=$1_area5 output=$1_area6 tool=rmdangle,rmsa,break,rmdupl,rmbridge thresh=-1,0,0,0,0 --verbose
+#v.generalize input=$1_area2 output=$1_area3 method=douglas threshold=50 --overwrite
+#v.generalize input=$1_area1 output=$1_area2 method=douglas_reduction threshold=0.002 reduction=30
+#v.category input=$1_area3 output=$1_area4 option=del cat=-1
+#v.clean input=$1_area3 output=$1_area4 tool=snap,break,rmdupl, thres=0.002
+v.out.ogr input=$1_area2 type=area output=$2/$1_area_redux.shp format=ESRI_Shapefile 
+  ;;
+esac
