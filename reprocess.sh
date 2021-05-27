14ad908226ff2855e8b2e41c6fbfc041c549bd4b
Philipp Wollschlegel <folken@kabelsalat.ch>
2021-05-27 21:07:37 +0200

    current state of work

--- reprocess.sh
+++ reprocess.sh
@@ -3,14 +3,19 @@ if [ -z "$1" ]; then
   echo specify file without extension
   exit 1
 fi
-rm -rf workdir/"$1"
+#rm -rf workdir/"$1"
 set -e 
-./01_convertgz $1
-./02_convertformat $1
+#./01_convertgz $1
+#./02_convertformat $1
+echo '### DROPAUTHOR'
 ./03_dropauthor $1
+echo '### FILTERo5m'
 ./04_filtero5m $1
+echo '### CONVERTTOSHP'
 ./05_converttoshp $1
+echo '### READSHP'
 ./06_readshape $1
+echo '### REDUCESHP'
 ./07_reduceshp $1
+echo '### PACKING'
 ./zz_packup $1
-#./zz_test $1
